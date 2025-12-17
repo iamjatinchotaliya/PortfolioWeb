cdb56af20b995fa3e6f01c9c2ef0d4e59f5ac129
Jatin Chotaliya <jatincchotaliya@gmail.com>
2025-12-17 10:32:59 +0530

    commit 3

--- lib/presentation/widgets/sections/hero_section.dart
+++ lib/presentation/widgets/sections/hero_section.dart
@@ -22,15 +22,7 @@ class HeroSection extends StatefulWidget {
 class _HeroSectionState extends State<HeroSection> with SingleTickerProviderStateMixin {
   late AnimationController _controller;
   int _currentTextIndex = 0;
-  final List<String> _rotatingTexts = [
-    'Flutter Developer',
-    'Full Stack Developer',
-    'Problem Solver',
-    'Creative Thinker',
-    'Creative Thinker',
-    'Creative Thinker',
-    'Creative Thinker',
-  ];
+  final List<String> _rotatingTexts = ['Flutter Developer', 'Python Enthusiast', 'Full Stack Developer', 'Problem Solver', 'Creative Thinker'];
 
   @override
   void initState() {
